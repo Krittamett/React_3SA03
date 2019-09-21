9b18ac7849e773f3cdaba3ae7bba25a75470cd73
Krittamett <47778603+Krittamett@users.noreply.github.com>
2019-09-22 00:01:28 +0700

    Commit 9 : Show Name

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import WordCard from "./WordCard";
 import Timer from "./Timer";
-import AddName from "./AddName"
 import  Footer from './Footer';
 import './App.css';
 const word = "Hello"
@@ -9,18 +8,30 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      message: "Answer"
+      message: "Answer",
+      type:""
     }
     this.answer = this.answer.bind(this);
   }
+  setName(event){
+    this.setState({type:event.target.value})
+  }
 
+  begin(){
+    alert("BEGIN !!")
+}
   answer(){
     this.setState({message:"HELLO"});
   }
   render() { 
     return ( 
     <div className = "page">
-      <AddName/>
+      <div className="setname">
+                <p>Enter your name</p>
+                <input type="text" onChange={this.setName.bind(this)} />
+                <button onClick={this.begin}>Start game</button>
+                <p>Welcome : {this.state.type}</p>
+            </div>
       <WordCard value = {word}/>
       <Timer/>
       <div className="Center"><button onClick = {this.answer}>ANSWER HERE</button><p>{this.state.message}</p></div>
