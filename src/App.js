67480ea8a9593dc2f5d1049eeaa5c405f130aa34
Krittamett <47778603+Krittamett@users.noreply.github.com>
2019-09-21 22:54:49 +0700

    Commit 2 : Change interface format

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const word = "Hello"
 class App extends Component { 
   render() { 
     return ( 
-    <div className = "App">
+    <div className = "page">
       <WordCard value = {word}/>
     </div> 
     ); 
