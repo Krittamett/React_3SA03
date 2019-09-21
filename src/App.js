192b3691b8a508b91724686264851a65fd1b1b7d
Krittamett <47778603+Krittamett@users.noreply.github.com>
2019-09-21 23:51:11 +0700

    Commit 8 : Show Answer

--- src/App.js
+++ src/App.js
@@ -5,13 +5,25 @@ import AddName from "./AddName"
 import  Footer from './Footer';
 import './App.css';
 const word = "Hello"
-class App extends Component { 
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      message: "Answer"
+    }
+    this.answer = this.answer.bind(this);
+  }
+
+  answer(){
+    this.setState({message:"HELLO"});
+  }
   render() { 
     return ( 
     <div className = "page">
       <AddName/>
       <WordCard value = {word}/>
       <Timer/>
+      <div className="Center"><button onClick = {this.answer}>ANSWER HERE</button><p>{this.state.message}</p></div>
       <Footer/>
     </div> 
     ); 
